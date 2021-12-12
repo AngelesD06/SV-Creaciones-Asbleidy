5ea104d0dc3b81ff38bafd4440318e1a98ab30ff
alejandra1009 <92603242+alejandra1009@users.noreply.github.com>
2021-12-12 12:43:46 -0500

    Subido por Maryury

---
+++ Sprint3/Maryury Alejandra Medina Martinez/Gestión_productos/inicio_admin.java
@@ -0,0 +1,165 @@
+package creaciones_asbleidy;
+
+import com.sun.awt.AWTUtilities;
+import java.awt.Shape;
+import java.awt.geom.RoundRectangle2D;
+import javax.swing.JOptionPane;
+
+public class inicio_admin extends javax.swing.JFrame {
+
+    public inicio_admin() {
+        //Para quitar los botones de cerrar, minimizar y maximizar
+        this.setUndecorated(true);
+        initComponents();
+        //Para centrar la ventana
+        this.setLocationRelativeTo(this);
+        //Para redondear las esquinas
+        Shape forma = new RoundRectangle2D.Double(0, 0, this.getBounds().width, this.getBounds().height, 27, 27);
+        AWTUtilities.setWindowShape(this, forma);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        pnlPrincipal = new javax.swing.JPanel();
+        btnCerrar = new javax.swing.JButton();
+        lblInicio = new javax.swing.JLabel();
+        Separador = new javax.swing.JSeparator();
+        lblLogo = new javax.swing.JLabel();
+        lblInventario = new javax.swing.JLabel();
+        lblProductos = new javax.swing.JLabel();
+        btnInventario = new javax.swing.JLabel();
+        btnProductos = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        pnlPrincipal.setBackground(new java.awt.Color(255, 255, 255));
+        pnlPrincipal.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        btnCerrar.setBackground(new java.awt.Color(255, 255, 255));
+        btnCerrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/salir.png"))); // NOI18N
+        btnCerrar.setBorder(null);
+        btnCerrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCerrarActionPerformed(evt);
+            }
+        });
+        pnlPrincipal.add(btnCerrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 20, -1, -1));
+
+        lblInicio.setFont(new java.awt.Font("Pristina", 0, 55)); // NOI18N
+        lblInicio.setText("Inicio");
+        pnlPrincipal.add(lblInicio, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 120, -1, 76));
+        pnlPrincipal.add(Separador, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 100, 380, 30));
+
+        lblLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/marca.png"))); // NOI18N
+        pnlPrincipal.add(lblLogo, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, 180, 60));
+
+        lblInventario.setFont(new java.awt.Font("Calibri", 0, 14)); // NOI18N
+        lblInventario.setText("Inventario");
+        pnlPrincipal.add(lblInventario, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 300, 60, -1));
+
+        lblProductos.setFont(new java.awt.Font("Calibri", 0, 14)); // NOI18N
+        lblProductos.setText("Productos");
+        pnlPrincipal.add(lblProductos, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 300, -1, -1));
+
+        btnInventario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/inventario.png"))); // NOI18N
+        btnInventario.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnInventarioMouseClicked(evt);
+            }
+        });
+        pnlPrincipal.add(btnInventario, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 200, 100, 90));
+
+        btnProductos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/productos.png"))); // NOI18N
+        btnProductos.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnProductosMouseClicked(evt);
+            }
+        });
+        pnlPrincipal.add(btnProductos, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 200, 80, 90));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pnlPrincipal, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pnlPrincipal, javax.swing.GroupLayout.PREFERRED_SIZE, 350, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnCerrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCerrarActionPerformed
+        //Se verifica que se desea cerrar la sesión
+        int r = JOptionPane.showConfirmDialog(null, "¿Está seguro de cerrar sesión?", "Cerrar", JOptionPane.YES_NO_OPTION);
+        if (r == JOptionPane.YES_OPTION) {
+            //Si la respuesta es sí, se cierra la sesión y se regresa al login
+            login l = new login();
+            l.setVisible(true);
+            dispose();
+        }
+    }//GEN-LAST:event_btnCerrarActionPerformed
+
+    private void btnProductosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnProductosMouseClicked
+        productos p = new productos();
+        p.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_btnProductosMouseClicked
+
+    private void btnInventarioMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnInventarioMouseClicked
+        inventario i = new inventario();
+        i.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_btnInventarioMouseClicked
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(inicio_admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(inicio_admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(inicio_admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(inicio_admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new inicio_admin().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JSeparator Separador;
+    private javax.swing.JButton btnCerrar;
+    private javax.swing.JLabel btnInventario;
+    private javax.swing.JLabel btnProductos;
+    private javax.swing.JLabel lblInicio;
+    private javax.swing.JLabel lblInventario;
+    private javax.swing.JLabel lblLogo;
+    private javax.swing.JLabel lblProductos;
+    private javax.swing.JPanel pnlPrincipal;
+    // End of variables declaration//GEN-END:variables
+}
